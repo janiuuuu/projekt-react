f38f03b9ad42360da8986774b1702a4bb0a8a8cc
janiuuuu <jan.borwin@gmail.com>
2021-10-18 21:11:47 +0200

    Zadanie 2 z prompt

---
+++ index_2.js
@@ -0,0 +1,19 @@
+console.log("Została wylosowana liczna");
+
+var randomNumber = Math.floor(Math.random() * (10 - 1)) + 1;
+
+console.log(randomNumber)
+
+
+var prompt= require('prompt');
+
+prompt.get(['number'], function (err, result) {
+    console.log('Została wpisana wartość :');
+    console.log('number: ' + result.number);
+
+    if(result.number == randomNumber ){s
+        console.log("Udało się.");
+    }else{
+        console.log("Sprobuj ponownie.")
+    }
+  });
