3947745d3888b0511452e65c444c7159023e7a70
janiuuuu <jan.borwin@gmail.com>
2021-10-27 20:23:17 +0200

    Poprawa formatowania.

--- time.js
+++ time.js
@@ -1,6 +1,5 @@
 const aktualnaData = new Date();
 
-
 const data = `${aktualnaData.getDate()} - ${(aktualnaData.getMonth().valueOf()+1)} - ${aktualnaData.getFullYear()}`;
 const czas = `${aktualnaData.getHours()} : ${aktualnaData.getMinutes()} : ${aktualnaData.getSeconds()}`;
 console.log(`aktualna data ${data} Aktulna godzina  ${czas}`);
